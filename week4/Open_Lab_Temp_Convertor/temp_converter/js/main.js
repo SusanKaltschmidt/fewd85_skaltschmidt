627b149c5a692934076d1324da35a300d0c1aba2
SusanKaltschmidt <susan@susan-kaltschmidt.de>
2016-05-21 16:33:04 -0400

    Labs, homeworks and missing things

---
+++ week4/Open_Lab_Temp_Convertor/temp_converter/js/main.js
@@ -0,0 +1,22 @@
+
+var tempC = 0;
+var tempF;
+
+tempF = (tempC * 1.8) + 32;
+
+
+
+if (tempF >= 451 || tempF <=32 ){
+	document.body.style.backgroundColor = "gray";
+	document.write("below");
+}
+
+if (tempF < 451 || tempF >32 ){
+	document.body.style.backgroundColor = "yellow";
+	document.write("above");
+}
+
+
+
+
+
---
+++ week4/Open_Lab_Temp_Convertor2/temp_converter/js/main.js
@@ -0,0 +1,42 @@
+
+
+/*
+var tempC = 0;
+var tempF;
+
+tempF = (tempC * 1.8) + 32;
+
+
+
+if (tempF >= 451 || tempF <=32 ){
+	document.body.style.backgroundColor = "gray";
+	document.write("below");
+}
+
+if (tempF < 451 || tempF >32 ){
+	document.body.style.backgroundColor = "yellow";
+	document.write("above");
+}
+
+
+
+
+
+*/
+
+var tempC = 0;
+var tempF;
+
+tempF = (tempC * 1.8) + 32;
+
+
+
+if (tempF >= 451 || tempF <=32 ){
+	document.body.style.backgroundColor = "gray";
+	document.write("below");
+}
+
+if (tempF < 451 || tempF >32 ){
+	document.body.style.backgroundColor = "yellow";
+	document.write("above");
+}
